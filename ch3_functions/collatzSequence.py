357a0a85ab850adcaa65567a751db36d5c1dc722
Marko-Temoc <jsemeria92@gmail.com>
2024-06-01 21:04:56 -0700

    Added collatzSequence.py
    Defined the function, but still gotta figure out the rest

---
+++ ch3_functions/collatzSequence.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def collatz(number):
+    if number % 2 == 0: #if even
+        number = number // 2
+    elif number % 2 != 0: #if odd
+        number = 3 * number + 1
+    print(number)
+    return number
+
+
+inp = int(input("Input an integer here: \n"))
+collatz(inp)
+
+#only does the thing once
