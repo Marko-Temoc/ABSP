58f5b92baf7b6315dad4e9624fb4169908da9ada
Marko-Temoc <52065824+Marko-Temoc@users.noreply.github.com>
2024-06-04 12:35:00 -0700

    Added tupleUnpacking.py

---
+++ ch4_lists/tupleUnpacking.py
@@ -0,0 +1,9 @@
+cat = ['fat', 'gray', 'loud']
+size = cat[0]
+color = cat[1]
+disposition = cat[2]
+#instead of doing the above,
+#we can use 'tuple unpacking'
+#as in this refactored example
+cat = ['fat', 'gray', 'loud']
+size, color, disposition = cat
