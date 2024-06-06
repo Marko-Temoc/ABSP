bd4454d4c6c943126f1c42e6063558ddd745ff6f
Marko-Temoc <jsemeria92@gmail.com>
2024-06-06 13:40:19 -0700

    Added file

--- ch4_lists/coinFlipping.py
+++ ch4_lists/coinFlipping.py
@@ -2,35 +2,37 @@
 
 import random
 
-#flip a coin 100 times and record results in this list
-flipResults = []
+numOfStreaks = 0
 
-for flip in range(100):
-    hort = random.randint(0,1)
-    if hort == 0:
-        flipResults.append('H')
-    if hort == 1:
-        flipResults.append('T')
-print(flipResults)
-#find out how many streaks of 6 heads or tails were in that list
-#look at first item, check if second item is the same
-#if it is the same, set counter to +1 (if counter is 5, win)
-#then check from the next position
-#if next item isn't the same, reset counter
-counter = 0
-streaks = 0
+for num in range(10000):
+    flipResults = []
 
-#took me a while to remember enumerate() was what I wanted
-for i, result in enumerate(flipResults):
-    #if we're on the last item
-    if i == len(flipResults) - 1:
-        break
-    if result == flipResults[i+1]: #if this item is same as next item
-        counter += 1
-        if counter == 5: #reset once we found 5 more that match
-            streaks += 1
+    for flip in range(100):
+        hort = random.randint(0,1)
+        if hort == 0:
+            flipResults.append('H')
+        if hort == 1:
+            flipResults.append('T')
+
+    counter = 0
+    streaks = 0
+
+    for i, result in enumerate(flipResults):
+        #if we're on the last item
+        if i == len(flipResults) - 1:
+            break
+        #if this item is same as next item
+        if result == flipResults[i+1]:
+            counter += 1
+            if counter == 5: #reset once we found 5 more that match
+                streaks += 1
+                counter = 0
+        else:
             counter = 0
-    else:
-        counter = 0
 
-print('Numbers of streaks: ' + str(streaks))
+    if streaks > 0:
+        numOfStreaks += 1
+        streaks = 0
+
+print('Chance of streak: %s%%' % (numOfStreaks/100))
+print('My chance of streak: ' + str(100*(numOfStreaks/10000)) + "%")
