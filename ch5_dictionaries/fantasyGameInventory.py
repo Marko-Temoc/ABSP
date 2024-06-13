0446fd383c18a06e3a655b2581b1188ef59c616e
Marko-Temoc <jsemeria92@gmail.com>
2024-06-13 13:42:45 -0700

    Finished fantasyGameInventory.py

--- ch5_dictionaries/fantasyGameInventory.py
+++ ch5_dictionaries/fantasyGameInventory.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 
-inv_warrior = {'battleaxe': 1, 'torch': 3, 'skulls': 4 ,'gold coin': 89}
-inv_archer = {'bow': 2, 'arrow': 20, 'quiver': 2, 'gold coin': 233}
-inv_mage = {'staff': 1, 'dagger': 3, 'scrolls': 11, 'gold coin': 28}
-
 dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 
 def displayInventory(inventory):
     totalNum = 0
-    print("Inventory of :")
+    print("Inventory of:")
     for item, num in inventory.items():
         print(item, num)
         totalNum += num
@@ -17,15 +13,19 @@ def displayInventory(inventory):
 def addToInventory(inventory, addedItems):
     #for every item in the loot list
     for item in addedItems:
-        #check if in inventory, if not add it with a value of 0
-        inventory.setdefault(item, 0)
-        #whether its in inventory or not, add 1 to the value
-        inventory[item] == inventory[item] + 1
+        #check if in inventory, if not add it with a value of 1
+        inventory.setdefault(item, 1)
     return inventory
 
 inv = {'gold coin': 42, 'rope': 1}
 inv = addToInventory(inv, dragonLoot)
 displayInventory(inv)
+
+#more inventory examples below
+
+#nv_warrior = {'battleaxe': 1, 'torch': 3, 'skulls': 4 ,'gold coin': 89}
+#nv_archer = {'bow': 2, 'arrow': 20, 'quiver': 2, 'gold coin': 233}
+#nv_mage = {'staff': 1, 'dagger': 3, 'scrolls': 11, 'gold coin': 28}
 
 #displayInventory(inv_warrior)
 #displayInventory(inv_archer)
