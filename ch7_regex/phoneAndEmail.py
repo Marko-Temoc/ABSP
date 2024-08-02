a990a631ed5478ff80174d3b04f87afec0e5c423
Marko-Temoc <jsemeria92@gmail.com>
2024-08-02 08:56:48 -0700

    fix anothor typo

--- ch7_regex/phoneAndEmail.py
+++ ch7_regex/phoneAndEmail.py
@@ -28,7 +28,7 @@ matches = []
 for groups in phoneRegex.findall(text):
     phoneNum = '-'.join([groups[1], groups[3], groups[5]])
     if groups[8] != '':
-        phoneNum += ' x' + gorups[8]
+        phoneNum += ' x' + groups[8]
     matches.append(phoneNum)
 for groups in emailRegex.findall(text):
     matches.append(groups[0])
