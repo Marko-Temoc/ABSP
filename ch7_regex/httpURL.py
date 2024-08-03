fcd24f0eec413657a9d60b7f344cb0a45c09b00d
Marko-Temoc <jsemeria92@gmail.com>
2024-08-03 15:47:51 -0700

    add todos to httpURL.py and made runnable

--- ch7_regex/httpURL.py
+++ ch7_regex/httpURL.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 import pyperclip, re
-httRegex = re.compile(r'''
+
+httRegex = re.compile(r'''(
     (https?://)
     (\w+)
-    (\.[a-zA-Z]{2,4})''', re.VERBOSE)
+    (\.[a-zA-Z]{2,4}))''', re.VERBOSE)
+
 #why do we have to make this a str type? what is it naturally?
 text = pyperclip.paste()
-print(type(text))
+
+# TODO find all matches in the text
+# TODO neatly format matches into a single string to paste as output
+# TODO display info if there were no matches
