d5de5ee507e3982387480cdef9c334ba4c126f69
Marko-Temoc <jsemeria92@gmail.com>
2024-08-04 12:05:13 -0700

    add optional www type field to regex

--- ch7_regex/httpURL.py
+++ ch7_regex/httpURL.py
@@ -4,15 +4,33 @@ import pyperclip, re
 
 httRegex = re.compile(r'''(
     (https?://)
+    ([a-zA-Z]{2,3}\.)?
     (\w+)
     (\.[a-zA-Z]{2,4}))''', re.VERBOSE)
 
-#why do we have to make this a str type? what is it naturally?
-text = pyperclip.paste()
+#take clipboard contents and put in text var
+text = str(pyperclip.paste())
+#init empty list to store our matches into as strs
+matches = []
+# TODO find all the url addresses that were found
+#take every tuple in the list, reconstruct into the html, and add to a list
+mo = httRegex.findall(text)
+print(mo)
+# TODO put all the results in one tidy string with newlines at the end
+# TODO print out string if matches were there, and print something else otherwise
+
+
+
+
+
+
+
+
+
 
 # TODO find all matches in the text
 #takes text, uses findall to output matches as tuples in a list, and for every tuple..
-for anymatch in httRegex.findall(text):
+#for anymatch in httRegex.findall(text):
     #recombine strings in tuple into one big string again
 
 # TODO neatly format matches into a single string to paste as output
