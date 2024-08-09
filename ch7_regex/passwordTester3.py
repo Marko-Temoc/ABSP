66c856604604246c666141956417a0f165c9bb3d
Marko-Temoc <jsemeria92@gmail.com>
2024-08-09 15:02:05 -0700

    add passwordTester3.py

--- ch7_regex/passwordTester3.py
+++ ch7_regex/passwordTester3.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python3
 
+import re
+
 def pstester(ps):
     oneDigitRegex = re.compile(r'\d+')
     oneLowerRegex = re.compile(r'.*[a-z].*')
     oneUpperRegex = re.compile(r'.*[A-Z].*')
     eightCharRegex = re.compile(r'.{8,}')
 
+    ps = str(ps)
     output = []
     shouldweoutput = 0
-    weak = "Weak password: must have at least"
+    weak = "Weak password: must have at least..."
     output.append(weak)
 
     if oneDigitRegex.search(ps) == None:
-        output.append(r'\n1 digit')
+        output.append('1 digit')
         shouldweoutput += 1
     if oneLowerRegex.search(ps) == None:
-        output.append(r'\n1 lower case letter')
+        output.append('1 lower case letter')
         shouldweoutput += 1
     if oneUpperRegex.search(ps) == None:
-        output.append(r'\n1 upper case letter')
+        output.append('1 upper case letter')
         shouldweoutput += 1
     if eightCharRegex.search(ps) == None:
-        output.append(r'\n8 characters')
+        output.append('8 characters')
         shouldweoutput += 1
     if shouldweoutput > 0:
         print('\n'.join(output))
