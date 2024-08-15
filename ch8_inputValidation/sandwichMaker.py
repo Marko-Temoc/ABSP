01aa436bd3f335ddf9e9ccba076611d86cbdc2e9
Marko-Temoc <jsemeria92@gmail.com>
2024-08-15 13:24:00 -0700

    Actually finish sandwichMaker.py

--- ch8_inputValidation/sandwichMaker.py
+++ ch8_inputValidation/sandwichMaker.py
@@ -1,17 +1,5 @@
 #!/usr/bin/env python3
 
-'''
-Write a program that asks users for their sandwich preferences. The program should use PyInputPlus to ensure that they enter valid input, such as:
-    Using inputMenu() for a bread type: wheat, white, or sourdough.
-    Using inputMenu() for a protein type: chicken, turkey, ham, or tofu.
-    Using inputYesNo() to ask if they want cheese.
-    If so, using inputMenu() to ask for a cheese type: cheddar, Swiss, or mozzarella.
-    Using inputYesNo() to ask if they want mayo, mustard, lettuce, or tomato.
-    Using inputInt() to ask how many sandwiches they want. Make sure this number is 1 or more.
-
-Come up with prices for each of these options, and have your program display a total cost after the user enters their selection.
-'''
-
 import pyinputplus as pyip
 
 def sandwichMaker():
@@ -26,13 +14,31 @@ def sandwichMaker():
     price += breads.get(bread_choice)
     protein_choice = pyip.inputMenu(list(proteins.keys()))
     price += proteins.get(protein_choice)
-    answer = pyip.inputYesNo('Would you like cheese on your sandwich?\n')
-    if answer == 'yes':
+    answer1 = pyip.inputYesNo('Would you like cheese on your sandwich?\n')
+    if answer1 == 'yes':
         cheese_choice = pyip.inputMenu(list(cheese_types.keys()))
         price += cheese_types.get(cheese_choice)
+    answer2 = pyip.inputYesNo('Would you like mayo, mustard, lettuce, or tomato?\n')
+    if answer2 == 'yes':
+        multiple = []
+        mayo_choice = pyip.inputYesNo('Mayo?\n')
+        if mayo_choice == 'yes':
+            multiple.append(' mayo')
+        mustard_choice = pyip.inputYesNo('Mustard?\n')
+        if mustard_choice == 'yes':
+            multiple.append(' mustard')
+        lettuce_choice = pyip.inputYesNo('Lettuce?\n')
+        if lettuce_choice == 'yes':
+            multiple.append(' lettuce')
+        tomato_choice = pyip.inputYesNo('Tomato?\n')
+        if tomato_choice == 'yes':
+            multiple.append(' tomato')
+        multiple = ','.join(multiple) + ','
+    if answer2 == 'no':
+        multiple = ''
     how_many_sandwiches = pyip.inputInt("How many of that sandwich do you want?\n", greaterThan=0)
     price *= how_many_sandwiches
     price = f'{price:.2f}'
-    print(f"All done! You want {how_many_sandwiches} sandwiches with {bread_choice} bread, {protein_choice} protein, and {cheese_choice} cheese!")
+    print(f"All done! You want {how_many_sandwiches} sandwiches with {bread_choice} bread, {protein_choice} protein,{multiple} and {cheese_choice} cheese!")
     print(f"\nYour total will be: ${price}")
 sandwichMaker()
