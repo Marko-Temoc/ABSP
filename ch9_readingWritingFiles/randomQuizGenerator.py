c9974ca96cb9c794f5b5b64d55d315f1b4483be7
Marko-Temoc <jsemeria92@gmail.com>
2024-08-24 08:21:11 -0700

    Add to randomQuizGenerator.py

--- ch9_readingWritingFiles/randomQuizGenerator.py
+++ ch9_readingWritingFiles/randomQuizGenerator.py
@@ -1,1 +1,34 @@
 #!/usr/bin/env python3
+# randomQuizGenerator.py - Creates quizzes with questions and answers in
+# random order, along with the answer key.
+import random
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
+   'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
+   'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
+   'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois':
+   'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas':
+   'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine':
+   'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan':
+   'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri':
+   'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada':
+   'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New
+   Mexico': 'Santa Fe', 'New York': 'Albany',
+   'North Carolina': 'Raleigh', 'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
+   'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
+   'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee':
+   'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':
+   'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West
+   Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+
+for quizNum in range(35):
+        #create a file called "capitalsquizX.txt" where X is the quiz number
+        quizFile = open(f'capitalsquiz{quizNum + 1}.txt', 'w')
+        #create a file called "capitalsquiz_answersX.txt" where X is the quiz number
+        answerKeyFile = open(f'capitalsquiz_answers{quizNum + 1}.txt', 'w')
+        #write out header for quiz
+        quizFile.write((' ' * 20) + f'State Capitals Quiz (Form{quizNum + 1})')
+        quizFile.write('\n\n')
+        #shuffle order of states
+        states = list(capitals.keys())
+        random.shuffles(states)
+       # TODO: Loop through all 50 states, making a question for each.
