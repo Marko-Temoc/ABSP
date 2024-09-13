e17f0f5e39802c4bc26f052e5cbdc245b00c04ae
Marko-Temoc <jsemeria92@gmail.com>
2024-09-13 13:58:38 -0700

    Update and finish searchpypi.py

--- ch12_webScraping/searchpypi.py
+++ ch12_webScraping/searchpypi.py
@@ -9,5 +9,11 @@ res = requests.get('https://google.com/search?q=' 'https://pypi.org/search/?q='
 res.raise_for_status()
 
 # TODO: Retrieve top search result links.
-
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
 # TODO: Open a browser tab for each result.
+linkElems = soup.select('.package-snippet')
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+    urlToOpen = 'https://pypi.org' + linkElems[i].get('href')
+    print('Opening', urlToOpen)
+    webbrowser.open(urlToOpen)
