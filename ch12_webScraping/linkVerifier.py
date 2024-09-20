a8f3f340fb9b17d602a1f6df288c1ead98a0a07d
Marko-Temoc <52065824+Marko-Temoc@users.noreply.github.com>
2024-09-20 12:10:24 -0700

    Implement basics of linkVerifier

--- ch12_webScraping/linkVerifier.py
+++ ch12_webScraping/linkVerifier.py
@@ -4,13 +4,28 @@
 #pass the p argument first to print the broken links but don't actually download the linked pages
 #TODO implement p feature
 
-import bs4, sys
+import bs4, requests, sys, os
 
 if len(sys.argv) > 2 :
     raise Exception('Only one argument allowed: please enter the full url')
 url = sys.argv[1]
 
-#TODO find every link on the page, probably searching a tags and their href attribute values
-#TODO use requests module to download and check for broken links
-#TODO download all these files to your current directory in a folder
-#TODO have a list of all broken links in a .txt file in that folder as well
+#find every link on the page and put into a list as Tag objects
+responseO = requests.get(url)
+responseO.raise_for_status()
+bsO = bs4.BeautifulSoup(url, 'html.parser')
+#list of Tag objects for each a element
+tagO = bs.select('a[href]')
+
+os.makedirs('./link_list')
+#iterate through tag list and try to download every link
+for tag in tag0:
+    link = tag.get('href')
+    linkResponse = requests.get(url + link)
+    linkResponse.raise_for_status()
+    if linkResponse.status_code == 404:
+        #TODO print a file in folder with list of 404 broken links
+        print(f'''{url}{link} returned a 404 error and wasn't found''')
+    writeFile = open(url + href + '.html', 'wb')
+    for chunk in linkResponse.iter_content(100000):
+        writeFile.write(chunk)
