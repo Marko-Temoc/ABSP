b48ff84ddaeb2773395b96d655770c2338831fef
Marko-Temoc <52065824+Marko-Temoc@users.noreply.github.com>
2024-10-22 16:17:01 -0700

    Update multipleTable.py

--- ch13_excelSpreadsheets/multipleTable.py
+++ ch13_excelSpreadsheets/multipleTable.py
@@ -2,5 +2,23 @@
 
 import openpyxl
 from openpyxl.utils import get_column_letter, column_index_from_string
+from openpyxl.styles import Font
+
+input = 6
 
 wb = openpyxl.Workbook()
+sheet = wb.active
+current_col = 1
+foont = Font(bold=True)
+
+#set up loop
+for i in range(1, input + 1):
+    sheet[f'A{i+1}'] = i
+    sheet[f'A{i+1}'].font = foont
+    current_col += 1
+    sheet[f'{get_column_letter(current_col)}1'] = i
+    sheet[f'{get_column_letter(current_col)}1'].font = foont
+
+
+
+wb.save('timesTable.xlsx')
